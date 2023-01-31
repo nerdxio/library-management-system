b4a80efcb1257c44820e03b9c89177d3808086d8
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-01-31 09:52:34 +0200

    add book entity and its repository ,service and contrller

---
+++ src/main/java/com/example/library/Controller/BookController.java
@@ -0,0 +1,54 @@
+package com.example.library.Controller;
+
+import com.example.library.model.Author;
+import com.example.library.model.Book;
+import com.example.library.service.AuthorService;
+import com.example.library.service.BookService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/author")
+public class BookController {
+
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Book>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Book> insert(@RequestBody @NotNull Book book) {
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(book.getId()).toUri();
+        return ResponseEntity.created(location).body(service.insert(book));
+    }
+
+    @PutMapping
+    public ResponseEntity<Book> update(@RequestBody Book book) {
+        service.update(book);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -1,7 +1,39 @@
 package com.example.library.service;
 
+import com.example.library.model.Author;
+import com.example.library.model.Book;
+import com.example.library.repository.AuthorRepository;
+import com.example.library.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    private final BookRepository repository;
+
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public Book findById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    public List<Book> findAll() {
+        return repository.findAll();
+    }
+
+    public Book insert(Book book) {
+        return repository.save(book);
+    }
+
+    public Book update(Book book) {
+        return repository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
 }
